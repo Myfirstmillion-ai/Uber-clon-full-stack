8e384335a6663194ddb53047aae6d6c9af0df694
Myfirstmillion-ai <lilnazx1115@gmail.com>
2025-12-02 13:05:50 -0500

    Actualizar CaptainDetails.jsx

--- Full-Stack-Uber-Clon-main/frontend/src/components/CaptainDetails.jsx
+++ Full-Stack-Uber-Clon-main/frontend/src/components/CaptainDetails.jsx
@@ -1,6 +1,5 @@
-
 import React, { useContext } from 'react'
-import { CaptainDataContext } from '../context/CapatainContext'
+import { CaptainDataContext } from '../context/CaptainContext'
 
 const CaptainDetails = () => {
 
@@ -10,8 +9,14 @@ const CaptainDetails = () => {
         <div>
             <div className='flex items-center justify-between'>
                 <div className='flex items-center justify-start gap-3'>
-                    <img className='h-10 w-10 rounded-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdlMd7stpWUCmjpfRjUsQ72xSWikidbgaI1w&s" alt="" />
-                    <h4 className='text-lg font-medium capitalize'>{captain.fullname.firstname + " " + captain.fullname.lastname}</h4>
+                    <div className='h-10 w-10 rounded-full bg-gradient-to-br from-orange-400 to-orange-600 flex items-center justify-center'>
+                        <span className='text-white font-bold text-sm'>
+                            {captain?.fullname?.firstname?.charAt(0)}{captain?.fullname?.lastname?.charAt(0)}
+                        </span>
+                    </div>
+                    <h4 className='text-lg font-medium capitalize'>
+                        {captain?.fullname?.firstname + " " + captain?.fullname?.lastname}
+                    </h4>
                 </div>
                 <div>
                     <h4 className='text-xl font-semibold'>$45.000 COP</h4>
