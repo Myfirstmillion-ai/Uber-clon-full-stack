4a0de58527669d179daa4765d1ace55125cd2353
Myfirstmillion-ai <lilnazx1115@gmail.com>
2025-12-02 10:58:09 -0500

    Actualizar CaptainHome.jsx

--- Full-Stack-Uber-Clon-main/frontend/src/pages/CaptainHome.jsx
+++ Full-Stack-Uber-Clon-main/frontend/src/pages/CaptainHome.jsx
@@ -7,7 +7,7 @@ import gsap from 'gsap'
 import ConfirmRidePopUp from '../components/ConfirmRidePopUp'
 import { useEffect, useContext } from 'react'
 import { SocketContext } from '../context/SocketContext'
-import { CaptainDataContext } from '../context/CapatainContext'
+import { CaptainDataContext } from '../context/CaptainContext'
 import axios from 'axios'
 import { LoadScript, GoogleMap, Marker } from '@react-google-maps/api'
 
